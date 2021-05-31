846fbf04f75ec7a83ce649d32e77e639f613874b
Zweli Nkuna <zwelimzweshiah@dutappfactory.co.za>
2021-06-01 01:13:18 +0200

    Revert "Adding the  Employee Index Logic"
    This reverts commit 7625e276e2ae346bd583f8ea9dd5fc474d8a1395.

--- EmployeeManagementSystem/Controllers/AccountController.cs
+++ EmployeeManagementSystem/Controllers/AccountController.cs
@@ -387,7 +387,8 @@ namespace EmployeeManagementSystem.Controllers
 
         //
         // POST: /Account/LogOff
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
